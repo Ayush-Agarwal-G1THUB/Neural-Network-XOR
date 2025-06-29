3d0202ef1a1de8b53a08fbb59db631e9e6d85742
Ayush Agarwal <ayushagarwal1203@gmail.com>
2025-06-29 23:02:41 +0530

    Update formatting

--- XORNeuralNetwork.cpp
+++ XORNeuralNetwork.cpp
@@ -3,8 +3,6 @@
 #include <vector>
 #include <random>
 
-using namespace std;
-
 struct point {
     int feature1;
     int feature2;
@@ -16,10 +14,11 @@ struct node {
     double activation;
     double bias;
     double error_signal;
-    vector<double> weightsIn;
-    vector<double> wGradients;
+    vector<double> weights_in;
+    vector<double> w_gradients;
 };
 
+using namespace std;
 using layer = vector<node>;
 
 /**
@@ -50,22 +49,22 @@ void loadSets (vector<point>& dataset, vector<point>& training_set, const int tr
 /**
  * @brief Creates the NN
  * @param nn Output : Stores the vector of layers, each a vector of nodes
- * @param nodesPerLayer Input : Stores the number of nodes in each layer of the NN
+ * @param nodes_per_layer Input : Stores the number of nodes in each layer of the NN
  * @param num_layers Stores the number of layers in the NN (including input, hidden and output)
  */
-void createNN (vector<layer> &nn, const vector<int> &nodesPerLayer, int &num_layers) {
-    num_layers = nodesPerLayer.size();
+void createNN (vector<layer> &nn, const vector<int> &nodes_per_layer, int &num_layers) {
+    num_layers = nodes_per_layer.size();
     nn.resize(num_layers);
 
-    for (int layerIdx = 0; layerIdx<num_layers; layerIdx++) {
-        nn[layerIdx].resize(nodesPerLayer[layerIdx]);
+    for (int layer_idx = 0; layer_idx<num_layers; layer_idx++) {
+        nn[layer_idx].resize(nodes_per_layer[layer_idx]);
 
-        if (layerIdx != 0) {    
-            layer& layer = nn[layerIdx];
+        if (layer_idx != 0) {    
+            layer& layer = nn[layer_idx];
 
             for (node &node : layer) {
-                node.weightsIn.resize(nn[layerIdx-1].size());
-                node.wGradients.resize(nn[layerIdx-1].size());
+                node.weights_in.resize(nn[layer_idx-1].size());
+                node.w_gradients.resize(nn[layer_idx-1].size());
             }
         }
         
@@ -84,10 +83,12 @@ double initRand () {
  * @param nn Stores the neural network
  */
 void initNN (vector<layer> &nn) {
-    for (int layerIdx = 1; layerIdx<nn.size(); layerIdx++) {
-        layer& layer = nn[layerIdx];
+    cout << "Initialising neural network..." << endl;
+
+    for (int layer_idx = 1; layer_idx<nn.size(); layer_idx++) {
+        layer& layer = nn[layer_idx];
         for (node& node : layer) {
-            for (double& w : node.weightsIn) {
+            for (double& w : node.weights_in) {
                 w = initRand();
             }
             node.bias = initRand();
@@ -98,27 +99,27 @@ void initNN (vector<layer> &nn) {
 /**
  * @brief Returns the z = sum(xi.wij)+bj for the node j
  * @param nn Neural Network
- * @param layerIdx Layer index in the neural network
- * @param nodeIdx Node index in the layer
+ * @param layer_idx Layer index in the neural network
+ * @param node_idx Node index in the layer
  */
-double calculateZ (vector<layer> &nn, int layerIdx, int nodeIdx) {
+double calculateZ (vector<layer> &nn, int layer_idx, int node_idx) {
     // Error handling
-    if (layerIdx == 0) {
+    if (layer_idx == 0) {
         cout << "Error : Trying to compute z value for input layer" << endl;
         return -1;
     }
 
-    node& currNode = nn[layerIdx][nodeIdx];
-    layer& prevLayer = nn[layerIdx-1];
+    node& curr_node = nn[layer_idx][node_idx];
+    layer& prev_layer = nn[layer_idx-1];
 
     double z = 0;
 
-    for (int i = 0; i<prevLayer.size(); i++) {
-        node& prevNode = prevLayer[i];
-        z += prevNode.activation * currNode.weightsIn[i];
+    for (int i = 0; i<prev_layer.size(); i++) {
+        node& prev_node = prev_layer[i];
+        z += prev_node.activation * curr_node.weights_in[i];
     }
 
-    z += currNode.bias;
+    z += curr_node.bias;
 
     return z;
 }
@@ -140,12 +141,12 @@ void forwardPropagate (const int num_layers, vector<layer> &nn, double a, double
     nn[0][0].activation = a;
     nn[0][1].activation = b;
 
-    for (int layerIdx = 1; layerIdx<num_layers; layerIdx++) {
-        layer& layer = nn[layerIdx];
-        for (int nodeIdx = 0; nodeIdx < layer.size(); nodeIdx++) {
-            node& node = layer[nodeIdx];
+    for (int layer_idx = 1; layer_idx<num_layers; layer_idx++) {
+        layer& layer = nn[layer_idx];
+        for (int node_idx = 0; node_idx < layer.size(); node_idx++) {
+            node& node = layer[node_idx];
 
-            node.z = calculateZ(nn, layerIdx, nodeIdx);
+            node.z = calculateZ(nn, layer_idx, node_idx);
             node.activation = sigmoid(node.z);
         }
     }
@@ -157,45 +158,45 @@ void forwardPropagate (const int num_layers, vector<layer> &nn, double a, double
  * @param learning_rate Input : Decides how fast the neural network learns
  */
 void backPropagate (vector<layer> &nn, const int num_layers, const int y, const double &learning_rate) {
-    for (int layeridx = num_layers-1; layeridx>0; layeridx--) {
-        layer &currLayer = nn[layeridx];
-        layer &prevLayer = nn[layeridx-1];
+    for (int layer_idx = num_layers-1; layer_idx>0; layer_idx--) {
+        layer &curr_layer = nn[layer_idx];
+        layer &prev_layer = nn[layer_idx-1];
 
         // Compute current layer error signals
-        if (layeridx == num_layers-1) { // Output layer
+        if (layer_idx == num_layers-1) { // Output layer
             // Calculated directly using the formula
-            layer& outputLayer = nn[num_layers-1];
-            node& outputNode = outputLayer[0];
-            outputNode.error_signal = outputNode.activation - y;
+            layer& output_layer = nn[num_layers-1];
+            node& output_node = output_layer[0];
+            output_node.error_signal = output_node.activation - y;
         }
         // For hidden layers, summation(deltaK.wHK) has already been computed by the forward layer
         // For output layer, (activation-label) has already been manually computed
-        for (node& node : currLayer) {
+        for (node& node : curr_layer) {
             node.error_signal *= node.activation * (1 - node.activation);
         }
 
-        for (node& node : prevLayer) node.error_signal = 0; 
+        for (node& node : prev_layer) node.error_signal = 0; 
         // Start the error signal in all nodes of the previous layer to 0
         // So that error_signal * weight can be summed as below
 
-        // In the nodes of currLayer, the error signal currently has the value of summation(error_signalK * weightHK)
+        // In the nodes of curr_layer, the error signal currently has the value of summation(error_signalK * weightHK)
         // Precomputed when parsing the forward layer
         // Or it is the output layer and has the manually computed error_signal
-        for (node& currNode : currLayer) {
+        for (node& curr_node : curr_layer) {
             // Update weights of current node
-            for (int i = 0; i<prevLayer.size(); i++) {
-                double& wGradientIJ = currNode.wGradients[i];
-                wGradientIJ = currNode.error_signal * prevLayer[i].activation; // delJ/delW
+            for (int i = 0; i<prev_layer.size(); i++) {
+                double& w_gradient_ij = curr_node.w_gradients[i];
+                w_gradient_ij = curr_node.error_signal * prev_layer[i].activation; // delJ/delW
 
-                currNode.weightsIn[i] -= learning_rate * wGradientIJ; // Update weight for current node
+                curr_node.weights_in[i] -= learning_rate * w_gradient_ij; // Update weight for current node
 
-                prevLayer[i].error_signal += currNode.error_signal * currNode.weightsIn[i]; 
+                prev_layer[i].error_signal += curr_node.error_signal * curr_node.weights_in[i]; 
                 // Summation step to update error_signal for the previous layer 
             }
 
             // Update bias of current node
-            double biasGradient = currNode.error_signal;
-            currNode.bias -= learning_rate * biasGradient;
+            double bias_gradient = curr_node.error_signal;
+            curr_node.bias -= learning_rate * bias_gradient;
         }
     }
 }
@@ -206,6 +207,8 @@ void backPropagate (vector<layer> &nn, const int num_layers, const int y, const 
  * @param nn Output : The trained neural network
  */
 void trainNN (const int num_layers, vector<point> training_set, vector<layer> &nn, const int num_epochs, const double learning_rate) {
+    cout << "Training..." << endl;
+    
     for (int epoch = 0; epoch < num_epochs; epoch++) {
         for (point point : training_set) {
             int a = point.feature1;
@@ -222,6 +225,7 @@ void trainNN (const int num_layers, vector<point> training_set, vector<layer> &n
  * @brief Evaluates the performance of the neural netword
  */
 void evaluateNN (vector<layer>& nn, vector<point>& test_set, const int& test_size, const int num_layers) {
+    cout << "Evaluating..." << endl;
     int correct = 0;
 
     for (point point : test_set) {
@@ -230,13 +234,18 @@ void evaluateNN (vector<layer>& nn, vector<point>& test_set, const int& test_siz
 
         forwardPropagate(num_layers, nn, a, b);
 
-        double outputVal = nn[num_layers-1][0].activation;
-        int prediction = (outputVal > 0.5) ? 1 : 0;
+        double output_val = nn[num_layers-1][0].activation;
+        int prediction = (output_val > 0.5) ? 1 : 0;
 
         int label = point.label;
 
         if (prediction == label) {
-            // cout << "Correct" << endl;
+            /* cout << "Correct" << endl;
+            cout << "Input Layer Node 1 = " << a << endl;
+            cout << "Input Layer Node 2 = " << b << endl;
+            cout << "Hidden Layer Node 1 = " << nn[1][0].activation << endl;
+            cout << "Hidden Layer Node 2 = " << nn[1][1].activation << endl;
+            cout << "Output Layer Node 1 = " << output_val << "\n" << endl; */
             correct++;
         }
         else {
@@ -264,11 +273,11 @@ int main()
     };
     
     const int training_size = 1000;
-    const int test_size = 100;
+    const int test_size = 50;
     const int num_epochs = 10000;
     const double learning_rate = 0.01;
     
-    const vector<int> nodesPerLayer = {2, 2, 1};
+    const vector<int> nodes_per_layer = {2, 2, 1};
     vector<point> training_set;
     vector<point> test_set;
     vector<layer> nn;
@@ -276,7 +285,7 @@ int main()
     int num_layers; // Including input, hidden and output layers
 
     loadSets(dataset, training_set, training_size, test_set, test_size);
-    createNN(nn, nodesPerLayer, num_layers);
+    createNN(nn, nodes_per_layer, num_layers);
     initNN(nn);
     trainNN(num_layers, training_set, nn, num_epochs, learning_rate);
     evaluateNN(nn, test_set, test_size, num_layers);
